de23888bf63c13ee151da7d0453c5454a975f8fb
Valentin Kalchev <vkalchev@outlook.com>
2023-04-12 15:40:55 +0100

    Covered URLEncodedBodyParameterEncoder.encode invalid parameters with unit test.

---
+++ Tests/NetworkingTests/URLEncodedBodyParameterEncoderTests.swift
@@ -0,0 +1,25 @@
+//
+//  URLEncodedBodyParameterEncoderTests.swift
+//  
+//
+//  Created by Valentin Kalchev on 12/04/2023.
+//
+
+import XCTest
+import Networking
+
+final class URLEncodedBodyParameterEncoderTests: XCTestCase {
+
+    func test_encode_invalidParameters_throwInvalidParametersTypeError() {
+        
+        let encoder = URLEncodedBodyParameterEncoder()
+
+        var request = URLRequest(url: someURL)
+        
+        XCTAssertThrowsError(try encoder.encode(parameters: "invalid format", forRequest: &request)) { error in
+            XCTAssertEqual(error as? ParameterEncodingError, ParameterEncodingError.invalidParametersType)
+        }
+    }
+}
+
+let someURL = URL(string: "https://someurl.com")!
